942da20c30c1798fbc8f7f8ef4c10a858fef4df8
AasthaSharma1809 <aash22cse@cmrit.ac.in>
2024-11-29 22:55:09 +0530

    Update process.php

--- process.php
+++ process.php
@@ -1,13 +1,13 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = htmlspecialchars($_POST['name']);
-  $email = htmlspecialchars($_POST['email']);
-  $phone = htmlspecialchars($_POST['phone']);
-  $dob = htmlspecialchars($_POST['dob']);
-  $gender = htmlspecialchars($_POST['gender']);
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $dob = $_POST['dob'];
+  $gender = $_POST['gender'];
 
   if (!empty($name) && !empty($email) && !empty($phone) && !empty($dob) && !empty($gender)) {
-    echo 'success'; // Success response for AJAX
+    echo 'success';
   } else {
     echo 'error';
   }
