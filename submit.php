5792f5e80a83329b96ffd275783f35da793e165e
AasthaSharma1809 <aash22cse@cmrit.ac.in>
2024-11-30 14:15:11 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -3,11 +3,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $phone = htmlspecialchars($_POST['phone']);
-    $address = htmlspecialchars($_POST['address']);
+    $address = htmlspecialchars($_POST['dob']);
 
     echo "Name: $name<br>";
     echo "Email: $email<br>";
     echo "Phone: $phone<br>";
-    echo "Address: $address<br>";
+    echo "Address: $dob<br>";
 }
 ?>
