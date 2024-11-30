02ce59f467c415df7c993405da6f10b1609d8349
AasthaSharma1809 <aash22cse@cmrit.ac.in>
2024-11-30 12:32:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
     $('#submitBtn').click(function() {
-        $('#resultName').text('Name: ' + $('#name').val());
-        $('#resultEmail').text('Email: ' + $('#email').val());
-        $('#resultPhone').text('Phone: ' + $('#phone').val());
-        $('#resultAddress').text('Address: ' + $('#address').val());
-        $('#resultGender').text('Gender: ' + $('#gender').val());
-        $('#result').show();
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var phone = $('#phone').val();
+        var address = $('#address').val();
+
+        if (name && email && phone && address) {
+            $('#resultName').text('Name: ' + name);
+            $('#resultEmail').text('Email: ' + email);
+            $('#resultPhone').text('Phone: ' + phone);
+            $('#resultAddress').text('Address: ' + address);
+            $('#result').show();
+        } else {
+            alert('Please fill all fields.');
+        }
     });
 
     $('#clearBtn').click(function() {
         $('#registrationForm')[0].reset();
         $('#result').hide();
-    });
+    });
 });
-
